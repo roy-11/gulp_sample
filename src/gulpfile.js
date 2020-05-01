0e16d122c91186b401af09e9fdd91003d859d7f6
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-05-01 16:17:01 +0900

    test gulp command & code

---
+++ src/gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require("gulp");
+
+gulp.task("message", () => {
+  return console.log("Gulp is running ...");
+});
+
+gulp.task("default", () => {
+  return console.log("Gulp is running ...");
+});
