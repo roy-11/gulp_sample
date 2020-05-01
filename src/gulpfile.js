7ce18798a1e46fafe9d2ffe9d6d745e661c31be8
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-05-01 16:20:40 +0900

    add copy code

--- src/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-
-gulp.task("message", () => {
-  return console.log("Gulp is running ...");
-});
-
-gulp.task("default", () => {
-  return console.log("Gulp is running ...");
-});
